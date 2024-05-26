4603c7071755c19a73b167bc6598ce7840900f11
biggus-developerus <74741815+biggus-developerus@users.noreply.github.com>
2024-05-26 06:12:44 +0400

    base package

---
+++ setup.py
@@ -0,0 +1,9 @@
+import os
+
+from setuptools import setup
+
+setup(
+    packages=[
+        os.path.join(root).replace("\\", ".") for root, _, files in os.walk("a2s_query") if "__init__.py" in files
+    ]
+)
